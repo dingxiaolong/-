ccced5acb54ae201f507a63a261019cbc3b143e8
xiaolong.ding <xiaolong.ding@useeinfo.com>
2017-10-25 16:18:47 +0800

    删除基本信息

--- MasonryTest/MasonryTest/ViewController.m
+++ MasonryTest/MasonryTest/ViewController.m
@@ -24,34 +24,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UIView *redView = [[UIView alloc] init];
-    redView.backgroundColor = [UIColor redColor];
-    [self.view addSubview:redView];
-    [redView makeConstraints:^(MASConstraintMaker *make) {
-        make.center.equalTo(self.view).offset(0);
-        make.width.height.equalTo(200);
-    }];
-    Person *person = [[Person alloc] init];
-    [person.eat1 eat];
-    [[person run1] eat1];
-    [person eat];
-    [person run];
-    [person run2]();
-    person.run2();
-    person.run3().eat3().run3().eat3();
-    
-    NSLog(@"我是为了测试sourceTree切换旧的版本才搞出来的额");
-    
-    NSLog(@"不切除新的分支看看能不能检出来这个呢,🙃和");
-    NSLog(@"hehehe");
-    NSLog(@"五月18号的测试");
-    NSLog(@"五月份测试的----");
-    
-    NSLog(@"五月份测试的---- 我是新切除的版本一");
-//   注释不能合并吗?
-
-    
-    person.eat4(@"麻辣火锅").run4(4).eat4(@"黄焖鸡米饭").run4(10);
 }
 
 - (void)didReceiveMemoryWarning {
